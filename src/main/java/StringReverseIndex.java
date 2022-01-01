8130eecc894dc8ad37e40170dee5c42485cb8e85
Vivek <vivekrana.2012@gmail.com>
2022-01-01 12:25:07 +0530

    string reverse

--- src/main/java/StringReverseIndex.java
+++ src/main/java/StringReverseIndex.java
@@ -1,7 +1,22 @@
-
 public class StringReverseIndex {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        String str = "Hello World";
+
+        System.out.println(str);
+
+        String reverse = reverseString(str);
+
+        System.out.println(reverse);
+    }
+
+    private static String reverseString(String str) {
+
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        return reverseString(str.substring(1)) + str.charAt(0);
     }
 }
